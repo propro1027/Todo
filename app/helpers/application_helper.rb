97b52dfcba55b30a0a77ea4f6c627014a7bdce1c
propro1027 <for.pro.business.address@gmail.com>
2021-01-14 00:20:18 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # カスタムヘルパー　2.9
+    def full_title(page_name="")
+        base_title="Todo"
+        if page_name.empty
+            base_title
+        else
+             page_name + " | " + base_title
+        end
+    end
 end
