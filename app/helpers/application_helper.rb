e4af1a6e6c8b2b814f1c9ec5d46514333c3510ce
propro1027 <for.pro.business.address@gmail.com>
2021-01-15 13:50:07 +0000

    Finish page-layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
     # カスタムヘルパー　2.9
     def full_title(page_name="")
         base_title="Todo"
-        if page_name.empty
+        if page_name.empty?
             base_title
         else
              page_name + " | " + base_title
